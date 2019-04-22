d543d3c885e83b0a98c24223f838a933d5f01ac8
Carla Romero <carlagromero@gmail.com>
2019-04-22 15:45:12 -0300

    Updates

---
+++ precache-manifest.32d56a5527a18084c70c32670b32bf6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6820fa104f55fc60299",
+    "url": "/events/static/js/runtime~main.b5bd43e3.js"
+  },
+  {
+    "revision": "fed7b1fbcc8f4a92cea4",
+    "url": "/events/static/js/main.af90bbb2.chunk.js"
+  },
+  {
+    "revision": "2f73ea753d7706ddce9c",
+    "url": "/events/static/js/2.66773f37.chunk.js"
+  },
+  {
+    "revision": "fed7b1fbcc8f4a92cea4",
+    "url": "/events/static/css/main.4b918eb4.chunk.css"
+  },
+  {
+    "revision": "14ef66848a1999fe2ae21c9f6cbfc297",
+    "url": "/events/index.html"
+  }
+];
